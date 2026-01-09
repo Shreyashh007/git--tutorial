9fd0d8e33ef61418d4110d9e152f82f6bd093cea
Shreyash Rajguru <shreyashrajguu@gmail.com>
2026-01-09 13:12:57 +0530

    Redesigned homepage

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+git commands-------------->
+1. git -v (to see the current version)
+2. git init (to initialze a repository/folder)
+3. git status (to see the current file status of file or code)
+4. git add.  (to add files in the staging step)
+5. git commit -m "inital commit(random message)" (this is to vcommit the files in the current repository) 
+6. git log (to view the history of commited changes within a git repository)\
+7. 
+'''
