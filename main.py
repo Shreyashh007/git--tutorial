5cb141de8502a126ff52d2d0955641470269a62c
REAPER urf.. shreyash <heyyinwwiwnib@gmail.com>
2026-01-10 13:36:40 +0530

    all file has been committed

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ git commands-------------->
 9. git config --list (show the added username,email, address etc and many other things)
 10.git config --global user.name "shreyash" (adds the username)
 11.git config --global user.email " shreyash.capricorn21@gmail.com"(adds the email)
-12.
+12.git clone (copy paste url or link) ---(clone a repository)\
+13.git diff(shows the exact changes made in the file) press Q to exit this state
+14.git restore <file name> (it restores the previous changes in the file you've made changes)
+15.git restore --staged <file name> (this will remove the file from staging process), in this the changes are no reverted. so to revert it back to the original state run "git restore" command
+16.git restore . (this is very important command as this will restore all the file to its original state.)
+17.git commit --amend -m " the message you want" (this will allow you to change an existing message of a specified file)
+18.
+19.
 '''
+
