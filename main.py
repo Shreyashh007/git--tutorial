5a298f4c2176c779652408472458c8eac9b64e74
REAPER urf.. shreyash <heyyinwwiwnib@gmail.com>
2026-01-10 12:24:10 +0530

    main.py has been initialized

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ git commands-------------->
 2. git init (to initialze a repository/folder)
 3. git status (to see the current file status of file or code)
 4. git add.  (to add files in the staging step)
-5. git commit -m "inital commit(random message)" (this is to vcommit the files in the current repository) 
-6. git log (to view the history of commited changes within a git repository)\
-7. 
+5. git commit -m "inital commit" (this is to commit the files in the current repository) 
+6. git commit -m "Redesigned Homepage" (this is to commit the files in the current repository) 
+7. git log (to view the history of commited changes within a git repository)
+8. git log --oneline (this is will display all the committed changes of log in a simplified manner) 
+9. git config --list (show the added username,email, address etc and many other things)
+10.git config --global user.name "shreyash" (adds the username)
+11.git config --global user.email " shreyash.capricorn21@gmail.com"(adds the email)
+12.
 '''
